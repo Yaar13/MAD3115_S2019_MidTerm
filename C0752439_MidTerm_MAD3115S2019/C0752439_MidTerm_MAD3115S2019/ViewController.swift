0d25c570575bd5f528825fdb291bb612cb5837af
parag <paraggarg66@yahoo.in>
2019-07-11 18:41:15 -0400

    Clone The Project And Started Working on the LaunchScreen.

---
+++ C0752439_MidTerm_MAD3115S2019/C0752439_MidTerm_MAD3115S2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0752439_MidTerm_MAD3115S2019
+//
+//  Created by Gurparkash Kaur on 2019-07-11.
+//  Copyright © 2019 Parag Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
